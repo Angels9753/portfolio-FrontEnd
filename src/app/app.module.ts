df9d2eeebeeefc5962b6dba4dbbd90ce945630d5
Angel Sapienza <angelsapienza@yahoo.com.ar>
2022-09-22 18:18:06 -0300

    favicon y botones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ConocemeComponent } from './components/conoceme/conoceme.component';
@@ -16,7 +15,6 @@ import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     LogoAPComponent,
     AcercaDeComponent,
     ConocemeComponent,
