62b096edf83221fee68631cb2469df1d14f2036c
Angel Sapienza <angelsapienza@yahoo.com.ar>
2023-01-13 11:07:18 -0300

    agregado de spinner

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { waitForAsync } from '@angular/core/testing';
 import { persona } from 'src/app/model/persona.model';
 import { PersonaService } from 'src/app/servicios/persona.service';
 import { TokenService } from 'src/app/servicios/token.service';
@@ -11,14 +12,18 @@ import { TokenService } from 'src/app/servicios/token.service';
 })
 export class AcercaDeComponent implements OnInit {
   pers: persona = null;
+  public loading : boolean;
 
   constructor(
     public personaService: PersonaService,
-    private tokenService: TokenService) {}
+    private tokenService: TokenService) {
+      this.loading=true;
+    }
 
   isLogged = false;
 
   ngOnInit(): void {
+    //this.Temporizador();
     this.cargarPersona();
     if (this.tokenService.getToken()) {
       this.isLogged = true;
@@ -29,9 +34,19 @@ export class AcercaDeComponent implements OnInit {
 
   cargarPersona(){
     this.personaService.detail(1).subscribe(data =>
-      {this.pers=data}
+      {this.pers=data;
+      if(!this.pers){
+        alert('Error en el servidor');
+      }else{this.loading=false;}
+    }
       )
   }
 
+  //Esta funcion es para probar en forma local el Spinner
+  Temporizador() {
+        setTimeout (() => {
+         this.cargarPersona();
+      }, 10000);
+  }
 
 }
